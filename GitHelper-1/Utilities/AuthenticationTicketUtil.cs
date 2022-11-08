f971e293a02686316d37d8febdaca4995b20821b
Mehdi Hossain <hossainmehdi03@gmail.com>
2022-11-08 16:55:25 +0530

    Add documentation for Utility classes in web api

--- GitHelper-1/Utilities/AuthenticationTicketUtil.cs
+++ GitHelper-1/Utilities/AuthenticationTicketUtil.cs
@@ -1,4 +1,13 @@
-﻿using GitHelper_1.Models;
+﻿/* 
+ Created By:        Mehdi Hossain
+ Created Date:      25-10-2022
+ Modified Date:     25-10-2022
+ Purpose:           Util to generate and decrypt ticket for login user. This ticket in later stored in cookie for further authorisation.
+ Purpose Type:      Utility class for ticket generation and decryption.
+ Referenced files:  Models\AuthenticationData.cs 
+ */
+
+using GitHelper_1.Models;
 using System;
 using System.Web.Security;
 
@@ -6,6 +15,14 @@ namespace GitHelper_1.Utilities
 {
     public static class AuthenticationTicketUtil
     {
+        /*
+            <summary>
+                create encrypted authentication ticket for corrosponding username and token.
+            </summary>
+            <param name="userName"> username for user authentication </param>
+            <param name="userToken"> github personal access token for user authentication </param> 
+            <returns>encrypted authentication token</returns>
+        */
         public static string createAuthenticationTicket(string userName, string userToken)
         {
             FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
@@ -22,6 +39,13 @@ namespace GitHelper_1.Utilities
             return encTicket;
         }
 
+        /*
+            <summary>
+                decrypts username and access token from encrypted authentication ticket
+            </summary>
+            <param name="value"> authentication ticket </param>
+            <returns>username, token encapsulated in AuthenticationData class</returns>
+        */
         public static AuthenticationData getAuthenticationDataFromTicket(string value)
         {
             FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(value);
--- GitHelper-1/Utilities/DateFormatter.cs
+++ GitHelper-1/Utilities/DateFormatter.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/* 
+ Created By:        Mehdi Hossain
+ Created Date:      24-10-2022
+ Modified Date:     25-10-2022
+ Purpose:           Util to convert datetime between utc and prefered user time offset.
+ Purpose Type:      Util to convert datetime between utc and prefered user time offset.
+ Referenced files:  NA
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,17 +19,33 @@ namespace GitHelper_1.Utilities
 
         private static Dictionary<string, TimeSpan> timeOffsetData = new Dictionary<string, TimeSpan>();
 
+
         static DateFormatter()
         {
             timeOffsetData.Add("IST", new TimeSpan(5, 30, 0));
             timeOffsetData.Add("UTC", TimeSpan.Zero);
         }
 
+        /*
+            <summary>
+                 used to get current utc datetime of the server.
+            </summary>
+            <param > none </param>
+            <returns>current utc datetimeoffset</returns>
+        */
         public static DateTimeOffset getCurrentUtcTime()
         {
             return DateTimeOffset.UtcNow;
         }
 
+
+        /*
+            <summary>
+                 used to get current datetime of server in user preference offset.
+            </summary>
+            <param > none </param>
+            <returns>current datetimeoffset in user preference offset</returns>
+        */
         public static DateTimeOffset getCurrentUserPrefTime(string offset = "IST")
         {
             TimeSpan dateOffset = timeOffsetData["UTC"];
@@ -31,6 +56,15 @@ namespace GitHelper_1.Utilities
             return getCurrentUtcTime().ToOffset(dateOffset);
         }
 
+
+        /*
+            <summary>
+                convert any datetimeoffset to user preference offset
+            </summary>
+            <param name="date"> date to be converted to user preference offset </param>
+            <param name="offset"> string that represent user preference offset </param>
+            <returns>user preference datetimeoffset</returns>
+        */
         public static DateTimeOffset ConvertToUserPref(DateTimeOffset date, string offset = "IST")
         {
             if (timeOffsetData.ContainsKey(offset))
@@ -43,12 +77,27 @@ namespace GitHelper_1.Utilities
             }
         }
 
+        /*
+            <summary>
+                convert any datetimeoffset to utc datetime
+            </summary>
+            <param name="date"> date to be converted to utc datetime </param>
+            <returns>utc datetimeoffset</returns>
+        */
         public static DateTimeOffset ConvertToUtc(DateTimeOffset date)
         {
 
             return date.ToOffset(timeOffsetData["UTC"]);
         }
 
+        /*
+            <summary>
+                create datetime in user preference offset
+            </summary>
+            <param name="date"> datetime to be created for user preference offset </param>
+            <param name="offset"> string that represent user preference offset </param>
+            <returns> datetime as per user preference offset </returns>
+        */
         public static DateTimeOffset CreateUserPrefDateTimeOffset(DateTime date, string offset = "IST")
         {
             TimeSpan dateOffset = timeOffsetData["UTC"];
@@ -59,6 +108,13 @@ namespace GitHelper_1.Utilities
             return new DateTimeOffset(date, dateOffset);
         }
 
+        /*
+            <summary>
+                create datetime in utc offset
+            </summary>
+            <param name="date"> datetime to be created for utc offset </param>
+            <returns> datetime as per utc offset </returns>
+        */
         public static DateTimeOffset CreateUTCDateTimeOffset(DateTime date)
         {
             TimeSpan dateOffset = timeOffsetData["UTC"];
