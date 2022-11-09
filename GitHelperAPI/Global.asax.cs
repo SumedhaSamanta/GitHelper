5210db8ed6a48d82f331747f91c77daf990388e3
SumedhaSamanta <sumedhasamanta@gmail.com>
2022-11-09 13:07:36 +0530

    remove BundleConfig

--- GitHelperAPI/Global.asax.cs
+++ GitHelperAPI/Global.asax.cs
@@ -17,7 +17,6 @@ namespace GitHelperAPI
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
         protected void Application_BeginRequest(Object sender, EventArgs e)
